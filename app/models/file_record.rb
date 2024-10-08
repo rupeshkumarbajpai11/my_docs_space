d18af8003cd0e6bf04421b6d5cb6d1bc7c40e9b6
rupesh <rupeshkumarbajpai11@gmail.com>
2024-10-08 23:33:38 +0530

    Setup CarrierWave for file uploads and added model validations

--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -1,3 +1,14 @@
 class FileRecord < ApplicationRecord
+  # uploader
+  mount_uploader :my_file, FileUploader
+
+  # associations
+  has_one :shared_url, dependent: :destroy 
   belongs_to :user
+  
+  # validations
+  validates :title, presence: true
+  validates :my_file, presence: true
+  
+
 end
--- app/models/shared_url.rb
+++ app/models/shared_url.rb
@@ -1,3 +1,16 @@
 class SharedUrl < ApplicationRecord
+  
+  # associations
   belongs_to :file_record
+
+  # validations
+  validates :shared_url, presence: true, uniqueness: true
+  # Generate the unique URL before creating the record
+  before_validation :generate_shared_url, on: :create
+
+  private
+  # Method to generate a unique shared URL
+  def generate_shared_url
+    self.shared_url ||= SecureRandom.hex(10)  # Generate a 10-character hex string
+  end
 end
